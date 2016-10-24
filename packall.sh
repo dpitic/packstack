2311e30d6d4a3032f4ae61d0301aeab6e6003619
Dan Pitic <dpitic@gmail.com>
2016-10-24 13:54:43 +0800

    Removed demo project provisioning. Provides a cleaner Openstack deployment without the example external network.

--- packall.sh
+++ packall.sh
@@ -5,6 +5,7 @@
 
 packstack \
     --allinone \
+    --provision-demo=n \
     --os-neutron-ovs-bridge-mappings=extnet:br-ex \
     --os-neutron-ovs-bridge-interfaces=br-ex:enp6s0 \
     --os-neutron-ml2-type-drivers=vxlan,flat
